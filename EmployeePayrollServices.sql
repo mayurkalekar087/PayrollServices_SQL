d447b885e4d2cc37415d87a82a6105be007832de
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-09-04 13:40:19 +0530

    feat:Ability To Create Table For Employee Payroll Services

--- EmployeePayrollServices.sql
+++ EmployeePayrollServices.sql
@@ -2,3 +2,12 @@ create database payroll_service
 use payroll_service
 
 select DB_NAME();
+
+create table employee_payroll
+(
+ id int identity(1,1) primary key,
+ name varchar(100) not null,
+ salary money not null,
+ start date not null
+);
+select * from employee_payroll;
