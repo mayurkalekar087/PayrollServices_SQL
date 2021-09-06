719fdeb4cf70cc9969b8b06be93ec358199bc663
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-09-06 11:02:47 +0530

    Feat: Ability to make Terissa as part of

--- EmployeePayrollServicesERDesign.sql
+++ EmployeePayrollServicesERDesign.sql
@@ -15,9 +15,14 @@ alter table employee_payroll add deductions float;
 alter table employee_payroll add taxable_pay real;
 alter table employee_payroll add net_pay real;
 alter table employee_payroll add net real;
+alter table employee_payroll add tax money;
+update employee_payroll set department = 'sales' where name = 'Terisa' 
 
+alter table employee_payroll
+drop column net;
 
-
+insert into employee_payroll(name,department,Gender,salary,start) values
+('Terisa','Marketing','F', 200000.0,'2021-05-23')
 
 
 
